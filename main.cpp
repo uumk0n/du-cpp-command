a68cce2de2c1a8d4f80764466ce80e2420091881
uumkon <uumkon@gmail.com>
2023-08-10 12:20:20 +0500

    рефакторинг

--- main.cpp
+++ main.cpp
@@ -2,12 +2,44 @@
 #include <fstream>
 #include <filesystem>
 #include <iomanip>
-#include <vector>
 #include <chrono>
 #include <thread>
 #include <cstring>
 
-void processDirectory(const std::filesystem::path &path, uint64_t &totalSize, bool printSize, bool printAllFiles, bool blockSize, bool printOnlyFullsize)
+constexpr uint64_t BLOCK_SIZE = 512;
+constexpr int FILE_PATH_INDEX = 13;
+
+struct Options
+{
+    bool printSize = false;
+    bool printAllFiles = false;
+    bool printOnlyFullsize = false;
+    bool printFullsize = false;
+    bool blockSize = true;
+};
+
+void processFile(const std::filesystem::path &filePath, uint64_t &totalSize, const Options &options)
+{
+    uint64_t fileSize = std::filesystem::file_size(filePath);
+    totalSize += fileSize;
+
+    if (options.printSize)
+        std::cout << (options.blockSize ? (fileSize + BLOCK_SIZE - 1) / BLOCK_SIZE : fileSize) << "\t";
+
+    if (!options.printOnlyFullsize)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::cout << filePath;
+    }
+
+    if (options.printAllFiles)
+        std::cout << "\t" << fileSize << (options.blockSize ? " blocks" : " bytes");
+
+    if (!options.printOnlyFullsize)
+        std::cout << std::endl;
+}
+
+void processDirectory(const std::filesystem::path &path, uint64_t &totalSize, const Options &options)
 {
     for (const auto &entry : std::filesystem::directory_iterator(path))
     {
@@ -15,37 +47,11 @@ void processDirectory(const std::filesystem::path &path, uint64_t &totalSize, bo
         {
             if (std::filesystem::is_regular_file(entry))
             {
-                uint64_t fileSize = std::filesystem::file_size(entry);
-                totalSize += fileSize;
-
-                if (printSize)
-                {
-                    if (blockSize)
-                    {
-                        std::cout << (fileSize + 511) / 512 << "\t";
-                    }
-                    else
-                    {
-                        std::cout << fileSize << "\t";
-                    }
-                }
-
-                if (!printOnlyFullsize)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-                    std::cout << entry.path();
-                }
-                if (printAllFiles)
-                {
-                    std::cout << "\t" << fileSize << (blockSize ? " blocks" : " bytes");
-                }
-
-                if (!printOnlyFullsize)
-                    std::cout << std::endl;
+                processFile(entry.path(), totalSize, options);
             }
             else if (std::filesystem::is_directory(entry))
             {
-                processDirectory(entry, totalSize, printSize, printAllFiles, blockSize, printOnlyFullsize);
+                processDirectory(entry, totalSize, options);
             }
         }
         catch (const std::exception &e)
@@ -57,83 +63,73 @@ void processDirectory(const std::filesystem::path &path, uint64_t &totalSize, bo
 
 int main(int argc, char *argv[])
 {
-    bool printSize = false;
-    bool printAllFiles = false;
-    bool printOnlyFullsize = false;
-    bool printFullsize = false;
-
-    bool blockSize = true; // По умолчанию вывод в 512-байтных блоках
+    Options options;
     uint64_t totalSize = 0;
     int startIndex = 1;
 
-    // Обработка опций
     for (int i = 1; i < argc; ++i)
     {
-        if (strcmp(argv[i], "-b") == 0)
+        std::string arg = argv[i];
+
+        if (arg == "-b")
         {
-            printSize = true;
-            blockSize = false; // Параметр -b выводит точный размер в байтах
+            options.printSize = true;
+            options.blockSize = false;
         }
-        else if (strcmp(argv[i], "-a") == 0)
+        else if (arg == "-a")
         {
-            printAllFiles = true;
+            options.printAllFiles = true;
         }
-        else if (strcmp(argv[i], "-c") == 0)
+        else if (arg == "-c")
         {
-            printFullsize = true;
+            options.printFullsize = true;
         }
-        else if (strcmp(argv[i], "-s") == 0)
+        else if (arg == "-s")
         {
-            printOnlyFullsize = true;
+            options.printOnlyFullsize = true;
         }
-        else if (strncmp(argv[i], "--files-from=", 13) == 0)
+        else if (arg.find("--files-from=") == 0)
         {
-            // Извлечь путь к файлу с путями
-            std::string filePath = argv[2];
+            std::string filePath = arg.substr(FILE_PATH_INDEX);
             std::ifstream fileList(filePath);
             if (fileList.is_open())
             {
                 std::string line;
                 while (std::getline(fileList, line))
                 {
-                    processDirectory(line, totalSize, printSize, printAllFiles, blockSize, printOnlyFullsize);
+                    processDirectory(line, totalSize, options);
                 }
             }
             else
             {
                 std::cerr << "Unable to open file: " << filePath << std::endl;
-                return 0;
+                return 1;
             }
             fileList.close();
-            return 0;
+            return 1;
         }
         else
         {
-            // Этот аргумент не является опцией, начинается обработка путей
             startIndex = i;
             break;
         }
     }
 
-    // Обработка путей
     for (int i = startIndex; i < argc; ++i)
     {
         std::filesystem::path targetPath(argv[i]);
 
-        // Обработка пути
         if (!std::filesystem::exists(targetPath))
         {
             std::cerr << "Path does not exist: " << targetPath << std::endl;
             continue;
         }
 
-        processDirectory(targetPath, totalSize, printSize, printAllFiles, blockSize, printOnlyFullsize);
+        processDirectory(targetPath, totalSize, options);
     }
 
-    if (printOnlyFullsize || printFullsize)
-    {
-        std::cout << "Total size: " << totalSize << (blockSize ? " blocks" : " bytes") << std::endl;
-    }
+    if (options.printOnlyFullsize || options.printFullsize)
+        std::cout << "Total size: " << totalSize << (options.blockSize ? " blocks" : " bytes") << std::endl;
 
     return 0;
 }
